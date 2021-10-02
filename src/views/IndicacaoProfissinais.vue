56696c60d2ef5968dca31097845d3492ba316621
Th4uã <thauamengue@sou.faccat.br>
2021-10-02 12:29:02 -0300

    adição do card

--- src/views/IndicacaoProfissinais.vue
+++ src/views/IndicacaoProfissinais.vue
@@ -1,27 +1,61 @@
 <template>
-    <v-container>
-        <h2 class="text-h5 text-center mb-3 mt-5">Indicação de Profissionais</h2>
-    </v-container>
+  <v-container>
+    <h2 class="text-h5 text-center mb-3 mt-5">Indicação de Profissionais</h2>
+    <v-card class="mx-auto" max-width="344">
+      <v-img
+        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        height="200px"
+      ></v-img>
+
+      <v-card-title> Top western road trips </v-card-title>
+
+      <v-card-subtitle> 1,000 miles of wonder </v-card-subtitle>
+
+      <v-card-actions>
+        <v-btn color="orange lighten-2" text> Explore </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon @click="show = !show">
+          <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
+        </v-btn>
+      </v-card-actions>
+
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+
+          <v-card-text>
+            I'm a thing. But, like most politicians, he promised more than he
+            could deliver. You won't have time for sleeping, soldier, not with
+            all the bed making you'll be doing. Then we'll go with that data
+            file! Hey, you add a one and two zeros to that or we walk! You're
+            going to do his laundry? I've got to find a way to escape.
+          </v-card-text>
+        </div>
+      </v-expand-transition>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name: 'IndicacaoProfissionais',
-    data() {
-        return {
-            IndicacaoProfissinais: []
-        }
-    },
-    created() {
-        fetch('https://it3zxc-default-rtdb.firebaseio.com/setembroamarelo.json')
-            .then(response => response.json())
-            .then(json => {
-                this.IndicacaoProfissinais = json;
-            })
-    }
-}
+  name: "IndicacaoProfissionais",
+  data() {
+    return {
+      IndicacaoProfissinais: [],
+      show: false,
+    };
+  },
+  created() {
+    fetch("https://it3zxc-default-rtdb.firebaseio.com/setembroamarelo.json")
+      .then((response) => response.json())
+      .then((json) => {
+        this.IndicacaoProfissinais = json;
+      });
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
