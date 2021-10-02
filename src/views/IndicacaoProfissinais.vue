2227710af5120bfc03f3c1a60ed64d290e995d26
Th4uã <thauamengue@sou.faccat.br>
2021-10-02 12:00:16 -0300

    Adicionando Primeira Pagina

---
+++ src/views/IndicacaoProfissinais.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container>
+        <h2 class="text-h5 text-center mb-3 mt-5">Indicação de Profissionais</h2>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'IndicacaoProfissionais',
+    data() {
+        return {
+            IndicacaoProfissinais: []
+        }
+    },
+    created() {
+        fetch('https://it3zxc-default-rtdb.firebaseio.com/setembroamarelo.json')
+            .then(response => response.json())
+            .then(json => {
+                this.IndicacaoProfissinais = json;
+            })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
