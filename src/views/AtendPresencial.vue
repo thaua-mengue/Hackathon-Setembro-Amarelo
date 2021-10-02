4e244da37315b10b378bece1dbd708ff5aa42bc5
Guilherme Lorentz <79127327+guilorentz@users.noreply.github.com>
2021-10-02 12:38:25 -0400

    Update AtendPresencial.vue
    Adicionando informação aos cards

--- src/views/AtendPresencial.vue
+++ src/views/AtendPresencial.vue
@@ -3,56 +3,48 @@
     <h2 class="text-h5 text-center mb-3 mt-5">
       Atendimento Presencial
     </h2>
-    <div class="containercard">
+    <div class="containercard" v-for="atend of listaAtendPres" :key="atend.id">
+      <v-card class="mx-auto" max-width="344">
+        <v-img src="atend.imagem" height="200px"></v-img>
 
+        <v-card-title>
+          {{ atend.nome }}
+        </v-card-title>
+
+        <v-card-subtitle>
+          1,000 miles of wonder
+        </v-card-subtitle>
+
+        <v-card-actions>
+          <v-btn color="orange lighten-2" text>
+            Explore
+          </v-btn>
+
+          <v-spacer></v-spacer>
+
+          <v-btn icon @click="show = !show">
+            <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+
+        <v-expand-transition>
+          <div v-show="show">
+            <v-divider></v-divider>
+
+            <v-card-text>
+              {{ atend.descricao }}
+            </v-card-text>
+          </div>
+        </v-expand-transition>
+      </v-card>
     </div>
-    <v-card class="mx-auto" max-width="344">
-      <v-img
-        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-        height="200px"
-      ></v-img>
-
-      <v-card-title>
-        Top western road trips
-      </v-card-title>
-
-      <v-card-subtitle>
-        1,000 miles of wonder
-      </v-card-subtitle>
-
-      <v-card-actions>
-        <v-btn color="orange lighten-2" text>
-          Explore
-        </v-btn>
-
-        <v-spacer></v-spacer>
-
-        <v-btn icon @click="show = !show">
-          <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
-        </v-btn>
-      </v-card-actions>
-
-      <v-expand-transition>
-        <div v-show="show">
-          <v-divider></v-divider>
-
-          <v-card-text>
-            I'm a thing. But, like most politicians, he promised more than he
-            could deliver. You won't have time for sleeping, soldier, not with
-            all the bed making you'll be doing. Then we'll go with that data
-            file! Hey, you add a one and two zeros to that or we walk! You're
-            going to do his laundry? I've got to find a way to escape.
-          </v-card-text>
-        </div>
-      </v-expand-transition>
-    </v-card>
   </v-container>
 </template>
 
 <script>
 export default {
   name: "AtendPresencial",
-   data() {
+  data() {
     return {
       listaAtendPres: [],
     };
@@ -63,9 +55,8 @@ export default {
       .then((json) => {
         this.listaAtendPres = json;
       });
-  }
+  },
 };
-
 </script>
 
 <style scoped></style>
